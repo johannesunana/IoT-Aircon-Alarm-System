f3f19c073d5cccddc23ba2da7722d1c63e6891c9
Johannes Andrei Unana <99703415+johannesunana@users.noreply.github.com>
2023-01-16 21:20:25 +0800

    Add files via upload

--- esp32_beta_v2/esp32_beta_v2.ino
+++ esp32_beta_v2/esp32_beta_v2.ino
@@ -1,7 +1,7 @@
-// Integrated 20A current sensor and acceleromter
-// Using MySQL_MariaDB_Generic library
-// Board: ESP32 Dev Kit
-// Edited: January 11, 2023
+// Integrated data gathering module
+// Board: ESP32 Development Kit
+// Updated: January 2023
+// Created by: Johannes Unana, Melvin Sta. Rosa
 
 /* I. Libraries */
 #include <Wire.h>                 // I2C 
@@ -20,8 +20,6 @@
 #define   MYSQL_DEBUG_PORT      Serial
 #define   _MYSQL_LOGLEVEL_      3
 
-// const int ledPin =  2; // indicator that device connects and uploads
-
 /* III. Required variables for digital and analog sensors */
 // A) SHT31 temperature humidity sensor
 SHT31 sht;
@@ -29,25 +27,20 @@ bool enableHeater = false;
 uint32_t start;
 uint32_t stop;
 float tempReading_float, humidityReading_float;
-
 // B) ACS712 analog current sensor
-//ACS712  ACS(IOUT, 3.3, 4095, 185);
+ACS712  ACS(IOUT, 3.3, 4095, 185);
 float currentReading_float;
-
 // C) ADXL345 digital accelerometer
-//Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified();
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified();
 float x2, x3, y2, y3, z2, z3, r, r2;
 float d = 0;      // determining condition
 float dx, dy, dz, dr;
 float t, h;
-
 // D) FCM2630-C01 freon sensor
 float vout_adc, vref_adc; // initial values from analogRead
 float vout_float, vref_float;  // converted to volts
-
 // E) Device ID
 byte device_id = 1;
-
 // F) MySQL Connector
 // 1 Initialize MySQL_MariaDB_Generic library
 MySQL_Connection conn((Client *)&client);
@@ -75,32 +68,31 @@ void setup() {
   Wire.setClock(100000);
   sht.begin(SHT31_ADDRESS); // start SHT31
   uint16_t stat = sht.readStatus();
-  Serial.print(stat, HEX);
-  //  Serial.println();
-  //  if (!accel.begin()) {     // start accelerometer
-  //    Serial.println("No ADXL345 detected");
-  //    while (1);
-  //  }
-  //  accel.setRange(ADXL345_RANGE_16_G);
-  //  ACS.autoMidPoint();       // start current sensor
+  Serial.println();
+  if (!accel.begin()) {     // start accelerometer
+    Serial.println("No ADXL345 detected");
+    while (1);
+  }
+  accel.setRange(ADXL345_RANGE_16_G);
+  ACS.autoMidPoint();       // start current sensor
 }
 
 void loop() {
-  //  MYSQL_DISPLAY("Connecting to server");
-  //  if (conn.connectNonBlocking(server_addr, server_port, user, password) != RESULT_FAIL) {
-  //    MYSQL_DISPLAY("\nConnect success");
-  //
-  // functions
-  tempHumidity();
-  //    accelerometer();
-  //    current();
-  //    gasSensor();
-  //    runInsert();
-  //    conn.close();
-  //  }
+  MYSQL_DISPLAY("Connecting to server");
+  if (conn.connectNonBlocking(server_addr, server_port, user, password) != RESULT_FAIL) {
+    MYSQL_DISPLAY("\nConnect success");
 
-  //  else MYSQL_DISPLAY("\nConnect failed");
-  //  delay(10000);   // retry after 10 seconds
+    // functions
+    tempHumidity();
+    accelerometer();
+    current();
+    gasSensor();
+    runInsert();
+    conn.close();
+  }
+
+  else MYSQL_DISPLAY("\nConnect failed");
+  delay(10000);   // retry after 10 seconds
   //  End of loop
 }
 
@@ -108,128 +100,124 @@ void tempHumidity() {
   start = micros();
   sht.read();
   stop = micros();
-
-  Serial.print("\t");
-  Serial.print(stop - start);
-  Serial.print("\t");
   float float_tempReading = sht.getTemperature();
+  Serial.print("T = ");
   Serial.print(float_tempReading, 1);
-  Serial.print("\t");
+  Serial.print("C\tH = ");
   float float_humidityReading = sht.getHumidity();
-  Serial.println(float_humidityReading, 1);
+  Serial.print(float_humidityReading, 1);
+  Serial.println("%");
   delay(100);
 }
 
-//void accelerometer() { // c/o Melvin, do not touch
-//  sensors_event_t event;
-//  accel.getEvent(&event);
-//  if (d <= 0) {
-//    x2 = event.acceleration.x;      //Initial Position of X
-//    y2 = event.acceleration.y;      //Initial Position of y
-//    z2 = event.acceleration.z;      //Initial Position of z
-//    r = 0;
-//  }
-//
-//  dx = event.acceleration.x - x2;  //Changes in position of x
-//  dy = event.acceleration.y - y2;  //Changes in position of y
-//  dz = event.acceleration.z - z2;  //Changes in position of z
-//
-//  x3 = powf((dx), 2);    //acceleration of x with respect to the initial calibrated position
-//  y3 = powf((dy), 2);    //acceleration of y with respect to the initial calibrated position
-//  z3 = powf((dz), 2);    //acceleration of z with respect to the initial calibrated position
-//  if (d > 0) {
-//    r = sqrtf(x3 + y3 + z3);
-//    if (r <= 0.2) { //if the change is very minimal or negligible set value to 0
-//      r = 0;
-//    }
-//  }
-//  d++;
-//  delay(250);
-//  Serial.print("R = ");
-//  Serial.print(r);
-//  Serial.println();
-//}
-//
-//void current() {
-//  currentReading_float = ACS.mA_AC() / 1000;
-//  Serial.print("A: ");
-//  delay(100);
-//}
-//
-//void gasSensor() {
-//  vout_adc = analogRead(VOUT);
-//  vref_adc = analogRead(VREF);
-//  Serial.print("VOUT = ");
-//  Serial.print(vout_float);
-//  Serial.print("\t\tVREF");
-//  Serial.print(vref_float);
-//  delay(250);
-//
-//  //  dtostrf(vout_float, 4, 2, vout_char);
-//  //  dtostrf(vref_float, 4, 2, vref_char);
-//}
-//
-//void runInsert() {
-//  // Initiate the query class instance
-//  MySQL_Query query_mem = MySQL_Query(&conn);
-//
-//  if (conn.connected()) {
-//    // Convert floats to strings before insert
-//    // dtostf == double to string
-//    dtostrf(tempReading_float, 4, 2, temp_char);
-//    dtostrf(humidityReading_float, 4, 2, hmd_char);
-//    dtostrf(r, 4, 2, r_char);
-//    dtostrf(currentReading_float, 4, 2, current_char);
-//    dtostrf(vout_float, 4, 2, vref_char);
-//    dtostrf(vref_float, 4, 2, vref_char);
-//
-//    // Insert char strings to placeholders in single query string
-//    // sprintf == string print
-//    sprintf(query1, INSERT_TEMPHMD, database, table1, device_id, temp_char, hmd_char);
-//    sprintf(query2, INSERT_ACC, database, table2, device_id, r_char);
-//    sprintf(query3, INSERT_CURRENT, database, table3, device_id, current_char);
-//    sprintf(query4, INSERT_GAS, database, table4, device_id, vout_char, vref_char);
-//    /* char INSERT_GAS[] = "INSERT INTO %s.%s (device_id, vout_data, vref_data, vout_status, vref_status, alarm_status) VALUES (%d, %s, %s, %d, %d, %d)"; */
-//
-//    // Execute query1
-//    MYSQL_DISPLAY1("Query1", query1);
-//    // KH, check if valid before fetching
-//    if ( !query_mem.execute(query1) ) {
-//      MYSQL_DISPLAY("Query 1: Insert error");
-//    }
-//    else {
-//      MYSQL_DISPLAY("Query 1: Data Inserted.");
-//    }
-//
-//    // Execute query2
-//    MYSQL_DISPLAY1("Query2", query2);
-//    // KH, check if valid before fetching
-//    if ( !query_mem.execute(query2) ) {
-//      MYSQL_DISPLAY("Query 2: Insert error");
-//    }
-//    else {
-//      MYSQL_DISPLAY("Query 2: Data Inserted.");
-//    }
-//
-//    // Execute query3
-//    MYSQL_DISPLAY1("Query3", query3);
-//    // KH, check if valid before fetching
-//    if ( !query_mem.execute(query3) ) {
-//      MYSQL_DISPLAY("Query 3: Insert error");
-//    }
-//    else {
-//      MYSQL_DISPLAY("Query 3: Data Inserted.");
-//    }
-//
-//    // Execute query4
-//    MYSQL_DISPLAY1("Query4", query4);
-//    // KH, check if valid before fetching
-//    if ( !query_mem.execute(query4) ) {
-//      MYSQL_DISPLAY("Query 4: Insert error");
-//    }
-//    else {
-//      MYSQL_DISPLAY("Query 4: Data Inserted.");
-//    }
-//  }
-//  else MYSQL_DISPLAY("Disconnected from Server. Can't insert.");
-//}
+void accelerometer() { // c/o Melvin, do not touch
+  sensors_event_t event;
+  accel.getEvent(&event);
+  if (d <= 0) {
+    x2 = event.acceleration.x;      //Initial Position of X
+    y2 = event.acceleration.y;      //Initial Position of y
+    z2 = event.acceleration.z;      //Initial Position of z
+    r = 0;
+  }
+
+  dx = event.acceleration.x - x2;  //Changes in position of x
+  dy = event.acceleration.y - y2;  //Changes in position of y
+  dz = event.acceleration.z - z2;  //Changes in position of z
+
+  x3 = powf((dx), 2);    //acceleration of x with respect to the initial calibrated position
+  y3 = powf((dy), 2);    //acceleration of y with respect to the initial calibrated position
+  z3 = powf((dz), 2);    //acceleration of z with respect to the initial calibrated position
+  if (d > 0) {
+    r = sqrtf(x3 + y3 + z3);
+    if (r <= 0.2) { //if the change is very minimal or negligible set value to 0
+      r = 0;
+    }
+  }
+  d++;
+  delay(250);
+  Serial.print("R = ");
+  Serial.print(r);
+  Serial.println();
+}
+
+void current() {
+  currentReading_float = ACS.mA_AC() / 1000;
+  Serial.print("A: ");
+  delay(100);
+}
+
+void gasSensor() {
+  vout_adc = analogRead(VOUT);
+  vref_adc = analogRead(VREF);
+  Serial.print("VOUT = ");
+  Serial.print(vout_float);
+  Serial.print("\t\tVREF");
+  Serial.print(vref_float);
+  delay(250);
+  //  dtostrf(vout_float, 4, 2, vout_char);
+  //  dtostrf(vref_float, 4, 2, vref_char);
+}
+
+void runInsert() {
+  // Initiate the query class instance
+  MySQL_Query query_mem = MySQL_Query(&conn);
+
+  if (conn.connected()) {
+    // Convert floats to strings before insert
+    // dtostf == double to string
+    dtostrf(tempReading_float, 4, 2, temp_char);
+    dtostrf(humidityReading_float, 4, 2, hmd_char);
+    dtostrf(r, 4, 2, r_char);
+    dtostrf(currentReading_float, 4, 2, current_char);
+    dtostrf(vout_float, 4, 2, vref_char);
+    dtostrf(vref_float, 4, 2, vref_char);
+
+    // Insert char strings to placeholders in single query string
+    // sprintf == string print
+    sprintf(query1, INSERT_TEMPHMD, database, table1, device_id, temp_char, hmd_char);
+    sprintf(query2, INSERT_ACC, database, table2, device_id, r_char);
+    sprintf(query3, INSERT_CURRENT, database, table3, device_id, current_char);
+    sprintf(query4, INSERT_GAS, database, table4, device_id, vout_char, vref_char);
+
+    // Execute query1
+    MYSQL_DISPLAY1("Query1", query1);
+    // KH, check if valid before fetching
+    if ( !query_mem.execute(query1) ) {
+      MYSQL_DISPLAY("Query 1: Insert error");
+    }
+    else {
+      MYSQL_DISPLAY("Query 1: Data Inserted.");
+    }
+
+    // Execute query2
+    MYSQL_DISPLAY1("Query2", query2);
+    // KH, check if valid before fetching
+    if ( !query_mem.execute(query2) ) {
+      MYSQL_DISPLAY("Query 2: Insert error");
+    }
+    else {
+      MYSQL_DISPLAY("Query 2: Data Inserted.");
+    }
+
+    // Execute query3
+    MYSQL_DISPLAY1("Query3", query3);
+    // KH, check if valid before fetching
+    if ( !query_mem.execute(query3) ) {
+      MYSQL_DISPLAY("Query 3: Insert error");
+    }
+    else {
+      MYSQL_DISPLAY("Query 3: Data Inserted.");
+    }
+
+    // Execute query4
+    MYSQL_DISPLAY1("Query4", query4);
+    // KH, check if valid before fetching
+    if ( !query_mem.execute(query4) ) {
+      MYSQL_DISPLAY("Query 4: Insert error");
+    }
+    else {
+      MYSQL_DISPLAY("Query 4: Data Inserted.");
+    }
+  }
+  else MYSQL_DISPLAY("Disconnected from Server. Can't insert.");
+}
