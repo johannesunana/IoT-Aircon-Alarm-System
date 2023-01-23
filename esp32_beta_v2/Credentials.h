75f396c459a96e16f7d82eb73e941784e3843b6d
Johannes Andrei Unana <99703415+johannesunana@users.noreply.github.com>
2023-01-23 18:06:29 +0800

    Working version
    - Enable if-conditions for variables
    - Created normal/malfunction status conditions for gas sensor vout_status, vref_status, and alarm_status
    - Fix println() for certain sensor outputs
    - In char INSERT_GAS[] change to %d from %s for byte variables

--- esp32_beta_v2/Credentials.h
+++ esp32_beta_v2/Credentials.h
@@ -1,13 +1,13 @@
 #ifndef Credentials_h
 #define Credentials_h
 
-#define USING_HOST_NAME           false
+#define USING_HOST_NAME           true
 
 #if USING_HOST_NAME
   // char server_addr[] = "raspberrypi.local";
   char server_addr[] = "asusx505za.local";
 #else
-  IPAddress server_addr(192, 168, 254, 104);
+  IPAddress server_addr(192, 168, 254, 105);
 #endif
 
 uint16_t server_port = 3306;
@@ -17,7 +17,6 @@ char pass[] = "JVA102123";         // your network password
 
 char user[]         = "arduino";              // MySQL user login username
 char password[]     = "arduino";          // MySQL user login password
-
 char database[] = "adnu_acrms_3";
 
 #endif    //Credentials_h
